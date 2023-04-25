faff6bd7dbd27f81b6d4cf1629f2945d017c58f7
Saikrishna T Bijil <saisboxintech@gmail.com>
2023-04-25 22:58:31 +0530

    added city intergration

--- index.js
+++ index.js
@@ -56,6 +56,45 @@ app.get("/weather", (req, res) => {
     });
 });
 
+app.get('/weatherCity', (req, res) => {
+  let name = req.query.name;
+  // let url = `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${lat}&lon=${lon}&appid=${apiKey}`;
+  let url = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${name}&appid=${apiKey}`;
+  axios
+    .get(url)
+    .then((response) => {
+      console.log(response.data);
+      const sunriseDate = new Date(response.data.sys.sunrise * 1000);
+      const sunsetDate = new Date(response.data.sys.sunset * 1000);
+      const formattedSunriseTime = sunriseDate.toLocaleString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true,
+      });
+      const formattedSunsetTime = sunsetDate.toLocaleString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true,
+      });
+
+      res.json({
+        temp: Math.round(response.data.main.temp).toString(),
+        humidity: response.data.main.humidity.toString(),
+        description: response.data.weather[0].description,
+        feels_like: Math.round(response.data.main.feels_like).toString(),
+        sunrise: formattedSunriseTime,
+        sunset: formattedSunsetTime,
+        name: response.data.name,
+        lowest: Math.round(response.data.main.temp_min).toString(),
+        highest: Math.round(response.data.main.temp_max).toString(),
+      });
+    })
+    .catch((error) => {
+      console.log("Error while blasting ::: " + error);
+      res.sendStatus(404)
+    });
+})
+
 app.listen(PORT, () => {
   console.log(`Supernovacore has started spining`);
 });
